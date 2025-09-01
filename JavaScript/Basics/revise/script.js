55065192066af440bc0838cd359f1e463fa72c8f
Shivam461221 <shivamlowanshi@gmail.com>
2025-09-01 13:37:04 +0530

    JS revise

---
+++ JavaScript/Basics/revise/script.js
@@ -0,0 +1,92 @@
+console.log('Hello JS');
+
+// keywords - let, const, var
+
+//let name = 121212121212n;
+
+//console.log(typeof name);
+
+//Primitive data types
+
+// Number, String, Boolean, Null, Undefined, BigInt
+
+
+//Non-primitive data types
+
+//array, object, functions
+
+//variables - 
+
+//var- global variable,  reassignment, redeclaration, 
+
+//let-scope varaibles, reassignment, 
+
+//const-scope variables, 
+
+var course = 'AIFS';
+
+console.log(course);
+
+
+course = 'CDEC';
+
+console.log(course);
+
+var course = 23;
+
+console.log(course);
+
+
+// {
+//   var name = 'Ayush';  
+// }
+
+// console.log(name);
+
+// let age = 25;
+
+// console.log(age);
+
+// age = 28;
+
+// console.log(age);
+
+// let age = '26';
+
+// const age = 25;
+
+// age = 26;
+
+let x = 12;  //number
+
+
+
+let y = "dsdad1212@##@"  //string  '', "", ``
+
+let z = false;   //boolean - true, false
+
+let a = null;  //null
+
+let b;  //undefined
+
+let phone = 1212121212n;
+
+
+console.log(typeof phone);
+
+//operators 
+
+//Arithmatic operator - mathematical - addition, substraction, multiplication, division
+//comparision opertor
+//logical operator
+//exponentiation operator
+
+
+
+
+
+
+
+
+
+
